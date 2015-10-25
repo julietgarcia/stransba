883ec986eeebc65ac144f20b0767c4b5d772cc67
Juliet Garcia <juliet@julietgarcia.com>
2015-10-25 11:35:49 -0430

    To pass params with translations

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,14 @@ var languages = global.config.application.languages;
 
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: global.config.site.title, welcome: global.lang.site.welcome, hi: global.lang.site.hi });
+});
+
 router.get('/:lang('+ languages +')', function(req, res, next) {
     global.i18n.setLanguage(req.params.lang);
 
-
-  res.render('index', { welcome: global.lang.site.welcome });
+    res.render('index', { title: global.config.site.title, welcome: global.lang.site.welcome, hi: global.lang.site.hi });
 });
 
 module.exports = router;
